df422cbfe6aad8d93cbfbedaa51711b513edbb91
Peide Wang <peide.wang@xplain-data.com>
2022-05-08 15:42:23 +0200

    httpClient

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include <Arduino.h>
 #include "IRrecv.h"
 #include "IRsend.h"
-#include <ESP8266WiFi.h> 
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-const char* ssid     = "devolo-178";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "APYLKAGMLRSOYXUB"; 
+const char *ssid = "devolo-178"; // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "APYLKAGMLRSOYXUB";
 
+uint16_t RECV_PIN = D5;
+uint16_t SEND_PIN = D6;
 
-uint16_t RECV_PIN = D5; 
-uint16_t SEND_PIN = D6; 
+uint32_t message = (uint32_t)1;
+uint32_t device_id = (uint32_t)0;
 
-
-
-uint32_t message = (uint32_t) 1;
-uint32_t device_id = (uint32_t) 0;
+HTTPClient sender;
+WiFiClient wifiClient;
 
 #define LED D0
 
@@ -27,7 +28,6 @@ uint32_t device_id = (uint32_t) 0;
 #define BLUE 2
 #define GREEN 3
 #define CYAN 4
-
 
 IRrecv irrecv(RECV_PIN);
 IRsend irsend(SEND_PIN);
@@ -46,7 +46,6 @@ decode_results results;
 // B -> D3
 
 bool buttonState = false;
-
 
 void rgb(int color)
 {
@@ -71,7 +70,7 @@ void rgb(int color)
     analogWrite(redPin, 255);
     analogWrite(bluePin, 255);
     analogWrite(greenPin, 0);
-    break;  
+    break;
 
   default:
     analogWrite(redPin, 255);
@@ -80,7 +79,6 @@ void rgb(int color)
     break;
   }
 }
-
 
 void setup()
 {
@@ -96,29 +94,73 @@ void setup()
   irrecv.enableIRIn();
   irsend.begin();
 
-
-  WiFi.begin(ssid, password);             // Connect to the network
+  WiFi.begin(ssid, password); // Connect to the network
   Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
-
+  Serial.print(ssid);
+  Serial.println(" ...");
 
   rgb(RED);
 
   int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
     delay(1000);
-    Serial.print(++i); Serial.print(' ');
+    Serial.print(++i);
+    Serial.print(' ');
   }
 
-   Serial.print("connected!!");
-   Serial.println(WiFi.localIP());
-   IPAddress address = WiFi.localIP();
-   Serial.println(u32_t(address));  
-   device_id = u32_t(address);
-  
+  Serial.print("connected!!");
+  Serial.println(WiFi.localIP());
+  IPAddress address = WiFi.localIP();
+  Serial.println(u32_t(address));
+  device_id = u32_t(address);
 
+  // test http get request
+
+  if (sender.begin(wifiClient, "http://192.168.2.114:3000/records"))
+  {
+
+    // HTTP-Code der Response speichern
+    int httpCode = sender.GET();
+    Serial.println("Get success!!");
+
+    if (httpCode > 0)
+    {
+
+      // Anfrage wurde gesendet und Server hat geantwortet
+      // Info: Der HTTP-Code für 'OK' ist 200
+      if (httpCode == HTTP_CODE_OK)
+      {
+
+        // Hier wurden die Daten vom Server empfangen
+
+        // String vom Webseiteninhalt speichern
+        String payload = sender.getString();
+
+        // Hier kann mit dem Wert weitergearbeitet werden
+        // ist aber nicht unbedingt notwendig
+        Serial.println(payload);
+      }
+      Serial.print("httpCode!= OK");
+      Serial.print(httpCode);
+    }
+    else
+    {
+      // Falls HTTP-Error
+      Serial.print("Error happens");
+      Serial.print(httpCode);
+    }
+
+    // Wenn alles abgeschlossen ist, wird die Verbindung wieder beendet
+    sender.end();
+  }
+  else
+  {
+    Serial.printf("HTTP-Verbindung konnte nicht hergestellt werden!");
+  }
+
+  Serial.print("setup ended");
 }
-
 
 void loop()
 {
@@ -140,8 +182,8 @@ void loop()
 
   if (irrecv.decode(&results))
   {
-     Serial.print("results.value=");
-     Serial.println(results.value);
-    irrecv.resume();                                             // Receive the next value
+    Serial.print("results.value=");
+    Serial.println(results.value);
+    irrecv.resume(); // Receive the next value
   }
 }
